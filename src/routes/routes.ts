49a352e54301f0aab247e43d8a9fe82f6f5b980d
misterpuffin <snghaoren@gmail.com>
2022-12-31 00:01:01 +0800

    Add example to Login route

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,15 +1,17 @@
 import { Router, Request, Response } from 'express'
 import { handleLogin } from '../controllers/login'
 import { authenticate } from '@middlewares/auth.middleware'
-import bodyParser from 'body-parser'
+import { Validator } from 'express-json-validator-middleware'
 import {
   getBookings,
   createBooking,
   editBooking,
   deleteBooking,
 } from '../controllers/bookings'
+import { telegramAuthSchema } from '@interfaces/auth.interface'
 
 export const router: Router = Router()
+const { validate } = new Validator()
 
 // landing page
 router.get('/', (req: Request, res: Response) => {
@@ -24,4 +26,4 @@ router.put('/bookings', authenticate, editBooking)
 // delete a booking
 router.delete('/bookings', authenticate, deleteBooking)
 // login route
-router.post('/login', bodyParser.json(), handleLogin)
+router.post('/login', validate({ body: telegramAuthSchema }), handleLogin)
