b2c5550b1b978c73cf88e94fa999d6e29be2f1d8
parth-io <67057645+parth-io@users.noreply.github.com>
2023-06-14 12:43:52 +0700

    Update bookings to only be seeded in dev env

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -212,8 +212,6 @@ async function main() {
     await readUserOnOrg(getDevSheetName(userOnOrgSheet));
   }
 
-  const bookingData = generateBookingData(userOnOrgData, venueData, 20);
-
   console.log(`Start seeding ...`);
   console.log(`Start seeding venues...`);
   for (const u of venueData) {
@@ -246,13 +244,17 @@ async function main() {
   }
   console.log(`Seeding finished.`);
 
-  console.log(`Start seeding bookings...`);
-  for (const u of bookingData) {
-    try {
-      const booking = await addBooking(u);
-      console.log(`Added booking of id ${booking.id} created by user of id ${booking.userId}`);
-    } catch (error) {
-      console.log(error);
+  if (isDevEnv) {
+    const bookingData = generateBookingData(userOnOrgData, venueData, 20);
+
+    console.log(`Start seeding bookings...`);
+    for (const u of bookingData) {
+      try {
+        const booking = await addBooking(u);
+        console.log(`Added booking of id ${booking.id} created by user of id ${booking.userId}`);
+      } catch (error) {
+        console.log(error);
+      }
     }
   }
   console.log(`Seeding finished.`);
