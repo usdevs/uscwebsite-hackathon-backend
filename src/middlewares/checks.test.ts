4e5149471972dfcca7ac5000a1f4cffe802951f9
sheng <chengzhisheng2001@gmail.com>
2023-12-10 09:39:03 +0800

    fix: Fix broken syntax in checks test

--- src/middlewares/checks.test.ts
+++ src/middlewares/checks.test.ts
@@ -51,7 +51,7 @@ describe('check user is in organisation', () => {
     await setup()
   })
 
-  test('check succeeds', async () => {
+  test.only('check succeeds', async () => {
     await expect(checkUserinOrg(user, normalOrg)).resolves.toEqual(true)
   })
 
@@ -109,10 +109,11 @@ describe('check start and end time', () => {
 describe('check booking privelege', () => {
   test('check succeeds, normal user', async () => {
     const booking: BookingPayload = {
+      eventName: 'test event',
       start: new Date(2021, 1, 2, 22, 30),
       end: new Date(2021, 1, 2, 23, 0),
       userId: user.id,
-      orgId: normalOrg.id,
+      userOrgId: normalOrg.id,
       venueId: venue.id,
     }
 
@@ -121,10 +122,11 @@ describe('check booking privelege', () => {
 
   test('check succeeds, admin user', async () => {
     const booking: BookingPayload = {
+      eventName: 'test event',
       start: new Date(2021, 1, 2, 22, 30),
       end: new Date(2021, 1, 3, 23, 0),
       userId: admin.id,
-      orgId: adminOrg.id,
+      userOrgId: adminOrg.id,
       venueId: venue.id,
     }
 
@@ -133,10 +135,11 @@ describe('check booking privelege', () => {
 
   test('check fails', async () => {
     const booking: BookingPayload = {
+      eventName: 'test event',
       start: new Date(2021, 1, 2, 22, 30),
       end: new Date(2021, 1, 3, 23, 0),
       userId: user.id,
-      orgId: normalOrg.id,
+      userOrgId: normalOrg.id,
       venueId: venue.id,
     }
 
@@ -147,27 +150,30 @@ describe('check booking privelege', () => {
 describe('check conflicting booking', () => {
   // create a bookingpayload and add it to the database
   const booking: BookingPayload = {
+    eventName: 'test event',
     start: new Date(2021, 1, 2, 22, 30),
     end: new Date(2021, 1, 2, 23, 0),
     userId: user.id,
-    orgId: normalOrg.id,
+    userOrgId: normalOrg.id,
     venueId: venue.id,
   }
 
   const nonConflictingBooking: BookingPayload = {
+    eventName: 'test event',
     start: new Date(2021, 1, 3, 21, 30),
     end: new Date(2021, 1, 3, 23, 0),
     venueId: venue.id,
     userId: user.id,
-    orgId: normalOrg.id,
+    userOrgId: normalOrg.id,
   }
 
   const conflictingBooking: BookingPayload = {
+    eventName: 'test event',
     start: new Date(2021, 1, 2, 22, 30),
     end: new Date(2021, 1, 2, 23, 0),
     venueId: venue.id,
     userId: user.id,
-    orgId: normalOrg.id,
+    userOrgId: normalOrg.id,
   }
 
   test('check succeeds', async () => {
@@ -194,42 +200,47 @@ describe('check stacked bookings', () => {
   const date4 = new Date(date3.getTime() + DURATION_PER_SLOT * 60 * 1000)
 
   const existingbooking: BookingPayload = {
+    eventName: 'test event',
     start: date1,
     end: date2,
     userId: user.id,
-    orgId: normalOrg.id,
+    userOrgId: normalOrg.id,
     venueId: venue.id,
   }
 
   const validNewBooking: BookingPayload = {
+    eventName: 'test event',
     start: date3,
     end: date4,
     userId: user.id,
-    orgId: normalOrg.id,
+    userOrgId: normalOrg.id,
     venueId: venue.id,
   }
 
   const invalidNewBooking: BookingPayload = {
+    eventName: 'test event',
     start: date2,
     end: date3,
     userId: user.id,
-    orgId: normalOrg.id,
+    userOrgId: normalOrg.id,
     venueId: venue.id,
   }
 
   const existingAdminBooking: BookingPayload = {
+    eventName: 'test event',
     start: date2,
     end: date3,
     userId: admin.id,
-    orgId: adminOrg.id,
+    userOrgId: adminOrg.id,
     venueId: venue.id,
   }
 
   const validAdminBooking: BookingPayload = {
+    eventName: 'test event',
     start: date3,
     end: date4,
     userId: admin.id,
-    orgId: adminOrg.id,
+    userOrgId: adminOrg.id,
     venueId: venue.id,
   }
 
