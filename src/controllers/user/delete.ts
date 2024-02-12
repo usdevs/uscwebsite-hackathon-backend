d30fad795b9af6ed0cb97c2bece6d1f9a522b8b8
sheng <chengzhisheng2001@gmail.com>
2024-02-12 08:39:30 +0800

    refactor: rename param

--- src/controllers/user/delete.ts
+++ src/controllers/user/delete.ts
@@ -10,16 +10,21 @@ export async function deleteUser(
   req: RequestWithUser,
   res: Response
 ): Promise<void> {
-  const userToDelete = parseInt(req.params['id'], 10)
-  if (Number.isNaN(userToDelete)) {
+  const userToDeleteId = parseInt(req.params['id'], 10)
+  if (Number.isNaN(userToDeleteId)) {
     throw new HttpException('User id not found', HttpCode.BadRequest)
   }
+
   if (!req.user) {
     throw new HttpException('Requires authentication', HttpCode.Unauthorized)
   }
 
+  if (req.user.id === userToDeleteId) {
+    throw new HttpException('Cannot delete self', HttpCode.BadRequest)
+  }
+
   await Policy.Authorize(deleteUserAction, Policy.deleteUserPolicy(), req.user)
 
-  const user = await destroyUser(userToDelete)
+  const user = await destroyUser(userToDeleteId)
   res.status(200).json(user)
 }
